7a59eaca0263ad73b3945f711e79e4ab8514c97d
Lakkireddy Hemalatha <137905315+lakkireddyhema@users.noreply.github.com>
2024-05-23 11:56:09 +0530

    Update pdf_convertor.py

--- pdf_convertor.py
+++ pdf_convertor.py
@@ -1,6 +1,4 @@
 pip PyPDF2
-
-
 def pdf_to_text(pdf_path, text_path):
     try:
         with open(pdf_path, 'rb') as pdf_file:
@@ -9,17 +7,13 @@ def pdf_to_text(pdf_path, text_path):
             for page_num in range(reader.numPages):
                 page = reader.getPage(page_num)
                 text += page.extract_text()  # Updated method name
-
         with open(text_path, 'w') as text_file:
             text_file.write(text)
         print(f"Text has been successfully extracted and saved to {text_path}")
-
     except FileNotFoundError:
         print(f"The file {pdf_path} does not exist.")
     except Exception as e:
         print(f"An error occurred: {e}")
-
-
 if __name__ == "__main__":
     pdf_path = input("Enter the path to the PDF file: ")
     text_path = input("Enter the path to save the text file: ")
